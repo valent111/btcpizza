f854df4e7b4d677768570187441a7162ef5f5645
unknown <notascamtrustmebro@gmail.com>
2021-12-31 23:06:08 +0200

    Updates

---
+++ precache-manifest.4c005feac69cf915848e8a392a9e7efb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7962834768659af7326ab7a3d067b320",
+    "url": "/btcpizza/index.html"
+  },
+  {
+    "revision": "04e1c220a8ef4ca419e6",
+    "url": "/btcpizza/static/css/main.5fca151b.chunk.css"
+  },
+  {
+    "revision": "6db414b578bd361901c8",
+    "url": "/btcpizza/static/js/2.3313c4cc.chunk.js"
+  },
+  {
+    "revision": "04e1c220a8ef4ca419e6",
+    "url": "/btcpizza/static/js/main.4f144f9f.chunk.js"
+  },
+  {
+    "revision": "3ffec2c429746d262b50",
+    "url": "/btcpizza/static/js/runtime-main.28585434.js"
+  }
+]);
